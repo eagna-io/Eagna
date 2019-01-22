448b5a4e1fa7ad3ea11f8c78f2f3ca20bc333dc7
AtsukiTak <takatomgoo@gmail.com>
2019-01-23 03:24:40 +0900

    frontend : initialize

---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?modules'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public/js',
+    publicPath: '/js',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './public',
+    hot: true
+  }
+};
