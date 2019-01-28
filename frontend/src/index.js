af41f0136c7620ad9120272b66a5e36b2140c9fd
AtsukiTak <takatomgoo@gmail.com>
2019-01-29 02:13:12 +0900

    frontend : persist Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,23 +4,37 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './reducers';
 import App from './app';
 
 const loggerMiddleware = createLogger();
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(
     thunkMiddleware,
     loggerMiddleware
   )
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate login={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('app')
 )
