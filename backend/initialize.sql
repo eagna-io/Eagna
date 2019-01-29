c8bef506cdd8dd5234fb0d6f6e0237cf87c4ec32
AtsukiTak <takatomgoo@gmail.com>
2019-01-29 12:36:12 +0900

    backend : add foreign key constraint

--- backend/initialize.sql
+++ backend/initialize.sql
@@ -8,5 +8,7 @@ CREATE TABLE access_tokens (
   token       text UNIQUE NOT NULL,
   user_name   text NOT NULL,
   is_valid    boolean NOT NULL,
-  created_at  integer NOT NULL
+  created_at  integer NOT NULL,
+  CONSTRAINT access_token_user_fkey FOREIGN KEY(user_name)
+    REFERENCES users(name) ON UPDATE CASCADE ON DELETE RESTRICT
 );
