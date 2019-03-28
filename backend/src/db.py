4626ccfa49bfed40a7fbbe653e2c1704aa73de35
AtsukiTak <takatomgoo@gmail.com>
2019-03-28 22:36:54 +0900

    connect to db via unix domain socket

--- backend/src/db.py
+++ backend/src/db.py
@@ -2,11 +2,21 @@ import os
 import psycopg2
 
 def connect_with_env():
-  uri = os.environ.get('DATABASE_URL')
-  return connect_with_uri(uri)
+  db_host = os.enviton['ROHAN_DB_HOST']
+  db_port = os.enviton['ROHAN_DB_PORT']
+  db_name = os.enviton['ROHAN_DB_NAME']
+  db_user = os.enviton['ROHAN_DB_USER']
+  db_pass = os.enviton['ROHAN_DB_PASS']
+  return connect(db_host, db_port, db_name, db_user, db_pass)
 
-def connect_with_uri(uri):
-  return psycopg2.connect(uri)
+def connect(db_host, db_port, db_name, db_user, db_pass):
+  return psycopg2.connect(
+    database = db_name,
+    host = db_host,
+    port = db_port,
+    user = db_user,
+    password = db_pass
+  )
 
 def query_all(conn, sql, args=()):
   with conn.cursor() as cur:
--- backend/src/main.py
+++ backend/src/main.py
@@ -10,9 +10,12 @@ p = Process(target = observe_market)
 p.start()
 print("Start to observe market open/close")
 
+# 環境変数の取得
+# 環境変数が指定されていない場合はエラー
+bind_host = os.environ['ROHAN_BIND_HOST']
+bind_port = os.environ['ROHAN_BIND_PORT']
+
 # API server の起動
-bind_host = os.getenv('BIND_HOST', '127.0.0.1')
-bind_port = os.getenv('BIND_PORT', 8000)
 print(f"Server start on {bind_host}:{bind_port}")
 server = Server(bind_host, bind_port)
 server.serve_forever()
