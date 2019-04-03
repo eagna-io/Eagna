8181e555658d4bc24d9bd2c9cc74090c679c334d
AtsukiTak <takatomgoo@gmail.com>
2019-04-03 23:54:32 +0900

    add header logo

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -6,35 +6,31 @@ import { Link } from 'react-router-dom';
 export default function Header(props) {
   return (
     <Container className={props.className}>
-      <Title>Rohan Market</Title>
+      <Link to="/"><Logo src="/img/logo.png" /></Link>
       <Profile to="/me"></Profile>
     </Container>
   );
 }
 
 const Container = styled.div`
+  display: flex;
   width: 100vw;
   height: 80px;
+  padding: 0 30px;
   background-color: #358ED7;
   border-bottom: 1px solid #979797;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-const Title = styled.div`
-  display: inline-block;
-  font-size: 26px;
-  font-family: "Americal Typewriter";
-  font-weight: bold;
-  color: white;
+const Logo = styled.img`
+  display: block;
   height: 80px;
-  line-height: 80px;
   margin-left: 30px;
 `;
 
 const Profile = styled(Link)`
-  display: inline-block;
-  position: absolute;
-  top: 16px;
-  right: 20px;
+  display: block;
   width: 48px;
   height: 48px;
   background-color: white;
