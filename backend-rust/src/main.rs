26aaa36da515e449febfbd8423010433617545d7
AtsukiTak <takatomgoo@gmail.com>
2019-05-28 14:12:46 +0900

    backend : takes BIND as environ variable

--- backend-rust/src/main.rs
+++ backend-rust/src/main.rs
@@ -2,5 +2,6 @@ use librohan::{app::ApiServer, infra::store::DbStoreFactory};
 
 fn main() {
     let store_factory = DbStoreFactory::new_with_env();
-    ApiServer::new(store_factory).run("localhost:8088");
+    let bind = std::env::var("BIND").expect("BIND is not specified");
+    ApiServer::new(store_factory).run(bind.as_str());
 }
