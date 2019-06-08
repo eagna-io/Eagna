5c6f37fd38f2a0798af81ac08940a2a33dce5b0f
AtsukiTak <takatomgoo@gmail.com>
2019-06-09 02:58:33 +0900

    remove frontend-old

--- frontend-old/src/context/access_token.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const AccessTokenContext = React.createContext({
-  accessToken: null,
-  setAccessToken: () => {},
-});
-
-export const RouterContext = React.createContext(null);
