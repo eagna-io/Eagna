13a9117950abcdf803142e435dc29e9ededc7c28
AtsukiTak <takatomgoo@gmail.com>
2019-06-27 19:59:58 +0900

    frontend : refactor admin/resolve_market page

--- frontend/src/pages/admin_resolve_market.tsx
+++ frontend/src/pages/admin_resolve_market.tsx
@@ -1,6 +1,5 @@
 import React, {FC, useState, useEffect} from 'react';
 import styled from 'styled-components';
-import moment from 'moment';
 
 import {getMarkets, resolveMarket} from 'api/market';
 import User from 'models/user';
