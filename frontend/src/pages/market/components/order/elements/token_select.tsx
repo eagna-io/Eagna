391c795fc9d8885c4bf5f51e93e26026bf413666
AtsukiTak <takatomgoo@gmail.com>
2019-06-27 14:03:45 +0900

    frontend : fix order component : token select bug

--- frontend/src/pages/market/components/order/elements/token_select.tsx
+++ frontend/src/pages/market/components/order/elements/token_select.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState, useCallback} from 'react';
+import React, {FC, useState, useCallback, useMemo} from 'react';
 import styled from 'styled-components';
 import {Token} from 'models/market';
 
@@ -10,36 +10,45 @@ interface Props {
 const Base: FC<Props> = React.memo(({tokens, onChange}) => {
   const [selected, setSelected] = useState<Token>(tokens[0]);
 
-  const Select = styled.select`
-    width: 100%;
-    height: 40px;
-    border: 1px solid #d1d5da;
-    border-radius: 4px;
-    background-color: white;
-    padding: 0 20px;
-    font-family: Lucida Grande;
-    font-size: 14px;
-    color: #37474f;
-  `;
+  const onChangeCallback = useCallback(
+    e => {
+      const token = tokens.find(t => t.name === e.target.value);
+      if (token) {
+        setSelected(token);
+        onChange(token);
+      }
+    },
+    [tokens, onChange, setSelected],
+  );
 
-  return (
-    <Select
-      name="token"
-      value={selected.name}
-      onChange={e => {
-        const token = tokens.find(t => t.name === e.target.value);
-        if (token) {
-          onChange(token);
-        }
-      }}>
-      {tokens.map(token => (
+  const selectOptions = useMemo(
+    () =>
+      tokens.map(token => (
         <option value={token.name} key={token.name}>
           {token.name}
         </option>
-      ))}
+      )),
+    [tokens],
+  );
+
+  return (
+    <Select name="token" value={selected.name} onChange={onChangeCallback}>
+      {selectOptions}
     </Select>
   );
 });
+
+const Select = styled.select`
+  width: 100%;
+  height: 40px;
+  border: 1px solid #d1d5da;
+  border-radius: 4px;
+  background-color: white;
+  padding: 0 20px;
+  font-family: Lucida Grande;
+  font-size: 14px;
+  color: #37474f;
+`;
 
 export const Pc = styled(Base)``;
 export const Mobile = styled(Base)``;
