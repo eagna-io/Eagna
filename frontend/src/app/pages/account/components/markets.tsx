6294be032330322fdc12fbaebcb5a02122767cb8
AtsukiTak <takatomgoo@gmail.com>
2019-09-16 01:34:41 +0900

    Topページの注目のマーケットのデザインをAccountページと合わせた

--- frontend/src/app/pages/account/components/markets.tsx
+++ frontend/src/app/pages/account/components/markets.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 import { Market } from "models/market";
 import { pc } from "app/components/responsive";
-import StatusBadge from "app/components/status_badge";
+import MarketCard from "app/components/market_card";
 
 interface MarketsComponentProps {
   title: string;
@@ -17,7 +16,7 @@ const MarketsComponent: FC<MarketsComponentProps> = ({ title, markets }) => {
       <Title>{title}</Title>
       <MarketsContainer>
         {markets.map(market => (
-          <MarketComponent key={market.id} market={market} />
+          <MarketCard key={market.id} market={market} />
         ))}
       </MarketsContainer>
     </Container>
@@ -49,78 +48,5 @@ const MarketsContainer = styled.div`
     white-space: nowrap;
     overflow: scroll;
     padding: 2px;
-  `)}
-`;
-
-const MarketComponent: FC<{
-  market: Market;
-}> = ({ market }) => {
-  return (
-    <MarketContainer to={`/market/${market.id}`}>
-      <MarketImage src={market.attrs.tokens[0].sumbnailUrl} />
-      <MarketStatusBadge status={market.status} />
-      <MarketTitle>{market.attrs.title}</MarketTitle>
-    </MarketContainer>
-  );
-};
-
-const MarketContainer = styled(Link)`
-  display: inline-block;
-  width: calc(50% - 6px);
-  height: 220px;
-  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.5);
-  border-radius: 4px;
-  margin-top: 20px;
-  vertical-align: top;
-  overflow: hidden;
-
-  &:nth-of-type(odd) {
-    margin-right: 12px;
-  }
-
-  ${pc(`
-    width: 250px;
-    height: 270px;
-    margin-right: 50px;
-    white-space: normal;
-
-    /* スマホの設定を打ち消す */
-    &:nth-of-type(odd) {
-      margin-right: 50px;
-    }
-  `)}
-`;
-
-const MarketImage = styled("div")<{ src: string }>`
-  display: block;
-  width: 100%;
-  height: 120px;
-  background-image: url(${props => props.src});
-  background-position: center;
-  background-size: cover;
-
-  ${pc(`
-    height: 150px;
-  `)}
-`;
-
-const MarketStatusBadge = styled(StatusBadge)`
-  margin-top: 10px;
-  margin-left: 6px;
-  width: 70px;
-  height: 18px;
-  font-size: 10px;
-  font-weight: normal;
-  line-height: 18px;
-`;
-
-const MarketTitle = styled.h4`
-  margin: 10px 0;
-  padding: 0 5px;
-  font-size: 12px;
-  font-weight: bold;
-
-  ${pc(`
-    font-size: 16px;
   `)}
 `;
--- frontend/src/app/pages/top/components/featured_market.tsx
+++ frontend/src/app/pages/top/components/featured_market.tsx
@@ -1,12 +1,11 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 import { Market, MarketStatus, MarketRepository } from "models/market";
-import { pc, MinPcWidth } from "app/components/responsive";
-import StatusBadge from "app/components/status_badge";
+import { pc } from "app/components/responsive";
+import MarketCard from "app/components/market_card";
 
-const FeaturedMarketListComponent: FC = () => {
+export default () => {
   const [markets, setMarkets] = useState<Market[]>([]);
 
   useEffect(() => {
@@ -21,7 +20,7 @@ const FeaturedMarketListComponent: FC = () => {
       <SectionTitle>注目のマーケット</SectionTitle>
       <MarketList>
         {markets.map(m => (
-          <MarketComponent key={m.id} market={m} />
+          <MarketCard key={m.id} market={m} />
         ))}
       </MarketList>
     </Container>
@@ -59,131 +58,14 @@ const SectionTitle = styled.h3`
 
 const MarketList = styled.div`
   width: 100%;
+  padding: 50px 12px;
 
   ${pc(`
-    width: ${MinPcWidth}px;
+    width: fit-content;
+    max-width: 100%;
     margin: 0 auto;
-  `)}
-`;
-
-const MarketComponent: FC<{ market: Market }> = React.memo(({ market }) => {
-  return (
-    <MarketContainer to={`/market/${market.id}`}>
-      <StatusBadge status={market.status} />
-      <Title>{market.attrs.title}</Title>
-      <HR />
-      <TokenContainer>
-        {market.attrs.tokens.map(token => (
-          <TokenItem key={token.name}>
-            <TokenName>{token.name}</TokenName>
-            <TokenPrice>-</TokenPrice>
-          </TokenItem>
-        ))}
-      </TokenContainer>
-    </MarketContainer>
-  );
-});
-
-export default FeaturedMarketListComponent;
-
-const MarketContainer = styled(Link)`
-  display: block;
-  width: calc(100% - 40px);
-  border: 1px solid #c2c6c9;
-  border-radius: 4px;
-  margin: 0 auto;
-  margin-top: 45px;
-  padding: 20px;
-  box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.5);
-
-  &:first-of-type {
-    margin-top: 30px;
-  }
-
-  ${pc(`
-    display: inline-block;
-    width: calc((100% - 50px) / 2);
-    padding: 30px;
-    vertical-align: top;
-    transition: transform 0.2s linear;
-
-    &:nth-of-type(even) {
-      margin-left: 50px;
-    }
-
-    &:first-of-type {
-      margin-top: 45px;
-    }
-
-    &:hover {
-      transform: scale(1.01);
-    }
-  `)}
-`;
-
-const Title = styled.div`
-  width: 100%;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 21px;
-  margin-top: 10px;
-
-  ${pc(`
-    font-size: 20px;
-    line-height: 30px;
-  `)}
-`;
-
-const HR = styled.hr`
-  width: 100%;
-  border: 0.5px solid #c2c6c9;
-  margin: 0;
-  margin-top: 18px;
-`;
-
-const TokenContainer = styled.div`
-  width: 100%;
-  padding: 0 12px;
-
-  ${pc(`
-    padding: 0 20px;
-  `)}
-`;
-
-const TokenItem = styled.div`
-  margin-top: 14px;
-
-  &:first-of-type {
-    margin-top: 20px;
-  }
-
-  ${pc(`
-    &:first-of-type {
-      margin-top: 25px;
-    }
-  `)}
-`;
-
-const TokenName = styled.div`
-  display: inline-block;
-  width: 80%;
-  font-size: 12px;
-  font-weight: 400;
-
-  ${pc(`
-    font-size: 18px;
-  `)}
-`;
-
-const TokenPrice = styled.div`
-  display: inline-block;
-  vertical-align: top;
-  width: 20%;
-  font-size: 12px;
-  font-weight: 400;
-  text-align: right;
-
-  ${pc(`
-    font-size: 18px;
+    padding: 50px 0 2px 20px
+    overflow-x: scroll;
+    white-space: nowrap;
   `)}
 `;
