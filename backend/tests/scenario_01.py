164bca67c936d59eeb10cc8792ddc2a4fa5d4e1a
AtsukiTak <takatomgoo@gmail.com>
2019-09-25 10:28:58 +0900

    test python scriptにassert_eq関数を追加

--- backend/tests/scenario_01.py
+++ backend/tests/scenario_01.py
@@ -21,43 +21,44 @@ def main():
 def get_user_test_1():
     headers = bearer_token({}, UserAccessToken)
     res = requests.get(url("/users/me/"), headers=headers)
-    assert res.status_code == 401
+    assert_eq(res.status_code, 401)
 
 def create_user_test():
+    # valid payload
     payload = {
         "name": "Hoge Hogeo",
         "email": "hoge@eagna.io",
     }
     headers = content_type_json(bearer_token({}, UserAccessToken))
     res = requests.post(url("/users/"), json.dumps(payload), headers=headers,)
-    assert res.status_code == 201, f"found status code {res.status_code}"
+    assert_eq(res.status_code, 201)
 
 def get_user_test_2():
     headers = bearer_token({}, UserAccessToken)
     res = requests.get(url("/users/me/"), headers=headers)
-    assert res.status_code == 200
-    assert res.json()["name"] == "Hoge Hogeo"
-    assert res.json()["email"] == "hoge@eagna.io"
-    assert res.json()["isAdmin"] == False
-    assert res.json()["point"] == 0
+    assert_eq(res.status_code, 200)
+    assert_eq(res.json()["name"], "Hoge Hogeo")
+    assert_eq(res.json()["email"], "hoge@eagna.io")
+    assert_eq(res.json()["isAdmin"], False)
+    assert_eq(res.json()["point"], 0)
 
 
 def get_markets_test_1():
     res = requests.get(url("/markets/"))
-    assert res.status_code == 200, "/markets/ へのリクエストに失敗"
-    assert len(res.json()) == 0, "/markets/ はまだ作成していないはず"
+    assert_eq(res.status_code, 200)
+    assert_eq(len(res.json()), 0)
 
 def create_market_test():
     # Test invalid access token
     invalid_token = "hoge"
     headers = bearer_token(empty_headers(), invalid_token)
     res = requests.post(url("/markets/"), headers=headers)
-    assert res.status_code == 401, f"found status code {res.status_code}"
+    assert_eq(res.status_code, 401)
 
     # Test valid access token but invalid payload
     headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
     res = requests.post(url("/markets/"), headers=headers)
-    assert res.status_code == 400, f"found status code {res.status_code}"
+    assert_eq(res.status_code, 400)
 
     # Valid request
     # でも↓ははじくように修正する必要ある
@@ -74,7 +75,7 @@ def create_market_test():
         'prizes': [],
     }
     res = requests.post(url("/markets/"), json.dumps(invalid_payload), headers=headers)
-    assert res.status_code == 201, f"found status code {res.status_code}"
+    assert_eq(res.status_code, 201)
 
 ## Utils
 def url(path):
@@ -90,5 +91,8 @@ def content_type_json(headers):
 def bearer_token(headers, token):
     headers['Authorization'] = f"Bearer: {token}"
     return headers
+
+def assert_eq(found, expected):
+    assert found == expected, f"expected [{expected}] but found [{found}]"
 
 main()
