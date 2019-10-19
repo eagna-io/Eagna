9f746696b507ad99d3932246d6444fe45ff6dd1a
AtsukiTak <takatomgoo@gmail.com>
2019-10-19 19:47:58 +0900

    test用データの修正

--- backend/tests/init_data.sql
+++ backend/tests/init_data.sql
@@ -2,5 +2,5 @@ INSERT INTO users (fb_uid, name, email, is_admin)
   VALUES ('test_admin', 'Test Admin', 'test-admin@eagna.io', True);
 INSERT INTO organizers (id, name, thumbnail_url)
   VALUES ('ec2966c5-d661-4a9b-b377-9e00f21d7dd4', 'Eagna', 'https://eagna.io');
-INSERT INTO prizes (id, name, description, thumbnail_url, price)
+INSERT INTO prizes (id, name, description, thumbnail_url, point)
   VALUES ('08c70349-2066-4e47-8841-ad5bdf49c90e', 'Starbucks Coffee フードチケット（300円分）', 'Starbucks Coffee フードチケット（300円分）', 'https://s3-ap-northeast-1.amazonaws.com/giftee-photos-tokyo-biz-2013/item_sku_photos/photos/3226/w_400px/sbjfood_01_260.jpg', 300);
