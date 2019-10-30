7444155524a8bdc4797a9d522d84ad043aae2686
AtsukiTak <takatomgoo@gmail.com>
2019-10-30 18:09:18 +0900

    format python code

--- backend/tests/scenarios/scenario_01.py
+++ backend/tests/scenarios/scenario_01.py
@@ -5,6 +5,7 @@ import json
 AdminAccessToken = "test_admin_access_token"
 UserAccessToken = "test_user_access_token"
 
+
 def main():
     # /users/ リソースのテスト
     test_users_api()
@@ -32,7 +33,11 @@ def test_users_api():
         "email": "hoge@eagna.io",
     }
     headers = content_type_json(bearer_token({}, UserAccessToken))
-    res = requests.post(url("/users/"), json.dumps(payload), headers=headers,)
+    res = requests.post(
+        url("/users/"),
+        json.dumps(payload),
+        headers=headers,
+    )
     assert_eq(res.status_code, 400)
 
     # 不正なペイロードではユーザーが作成できないことをテストする
@@ -42,7 +47,11 @@ def test_users_api():
         "email": "",
     }
     headers = content_type_json(bearer_token({}, UserAccessToken))
-    res = requests.post(url("/users/"), json.dumps(payload), headers=headers,)
+    res = requests.post(
+        url("/users/"),
+        json.dumps(payload),
+        headers=headers,
+    )
     assert_eq(res.status_code, 400)
 
     # 正しいペイロードでユーザーが作成できることをテストする
@@ -51,7 +60,11 @@ def test_users_api():
         "email": "hoge@eagna.io",
     }
     headers = content_type_json(bearer_token({}, UserAccessToken))
-    res = requests.post(url("/users/"), json.dumps(payload), headers=headers,)
+    res = requests.post(
+        url("/users/"),
+        json.dumps(payload),
+        headers=headers,
+    )
     assert_eq(res.status_code, 201)
 
     # ↑で作成したユーザーが取得できることをテストする
@@ -79,7 +92,8 @@ def test_prizes_api():
 
     # アクセストークンが正しくても、
     # 不正なペイロードでは賞品が作成できないことをテストする
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     res = requests.post(url("/prizes/"), headers=headers)
     assert_eq(res.status_code, 400)
 
@@ -142,7 +156,6 @@ def test_prizes_api():
     assert_eq(prize["available"], payload["available"])
 
 
-
 def test_markets_api():
     # 初期状態ではマーケットが存在しないことをテストする
     res = requests.get(url("/markets/"))
@@ -156,92 +169,124 @@ def test_markets_api():
 
     # アクセストークンが正しくても、
     # 不正なペイロードではマーケットが作成できないことをテストする
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     res = requests.post(url("/markets/"), headers=headers)
     assert_eq(res.status_code, 400)
 
     # 不正なペイロードではマーケットが作成できないことをテストする
     # タイトルが空文字列は許可しない
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     invalid_payload = {
-        'title': "",
-        'description': "hoge",
-        'organizerId': "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
-        'lmsrB': 0,
-        'open': datetime.utcnow().isoformat() + 'Z',
-        'close': datetime.utcnow().isoformat() + 'Z',
-        'totalRewardPoint': 10000,
-        'tokens': [],
-        'prizes': [],
+        "title": "",
+        "description": "hoge",
+        "organizerId": "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
+        "lmsrB": 0,
+        "open": datetime.utcnow().isoformat() + "Z",
+        "close": datetime.utcnow().isoformat() + "Z",
+        "totalRewardPoint": 10000,
+        "tokens": [],
+        "prizes": [],
     }
-    res = requests.post(url("/markets/"), json.dumps(invalid_payload), headers=headers)
+    res = requests.post(url("/markets/"),
+                        json.dumps(invalid_payload),
+                        headers=headers)
     assert_eq(res.status_code, 400)
 
     # 不正なペイロードではマーケットが作成できないことをテストする
     # トークンが空配列は許可しない
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     invalid_payload = {
-        'title': "hoge",
-        'description': "hoge",
-        'organizerId': "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
-        'lmsrB': 0,
-        'open': datetime.utcnow().isoformat() + 'Z',
-        'close': datetime.utcnow().isoformat() + 'Z',
-        'totalRewardPoint': 10000,
-        'tokens': [],
-        'prizes': [],
+        "title": "hoge",
+        "description": "hoge",
+        "organizerId": "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
+        "lmsrB": 0,
+        "open": datetime.utcnow().isoformat() + "Z",
+        "close": datetime.utcnow().isoformat() + "Z",
+        "totalRewardPoint": 10000,
+        "tokens": [],
+        "prizes": [],
     }
-    res = requests.post(url("/markets/"), json.dumps(invalid_payload), headers=headers)
+    res = requests.post(url("/markets/"),
+                        json.dumps(invalid_payload),
+                        headers=headers)
     assert_eq(res.status_code, 400)
 
     # 不正なペイロードではマーケットが作成できないことをテストする
     # トークン名が空文字列は許可しない
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     invalid_payload = {
-        'title': "hoge",
-        'description': "",
-        'organizerId': "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
-        'lmsrB': 0,
-        'open': datetime.utcnow().isoformat() + 'Z',
-        'close': datetime.utcnow().isoformat() + 'Z',
-        'totalRewardPoint': 10000,
-        'tokens': [{"name": "", "description": "", "thumbnailUrl": ""}],
-        'prizes': [],
+        "title": "hoge",
+        "description": "",
+        "organizerId": "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
+        "lmsrB": 0,
+        "open": datetime.utcnow().isoformat() + "Z",
+        "close": datetime.utcnow().isoformat() + "Z",
+        "totalRewardPoint": 10000,
+        "tokens": [{
+            "name": "",
+            "description": "",
+            "thumbnailUrl": ""
+        }],
+        "prizes": [],
     }
-    res = requests.post(url("/markets/"), json.dumps(invalid_payload), headers=headers)
+    res = requests.post(url("/markets/"),
+                        json.dumps(invalid_payload),
+                        headers=headers)
     assert_eq(res.status_code, 400)
 
     # 不正なペイロードではマーケットが作成できないことをテストする
     # Prizeが空配列は許可しない
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     invalid_payload = {
-        'title': "hoge",
-        'description': "hoge",
-        'organizerId': "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
-        'lmsrB': 0,
-        'open': datetime.utcnow().isoformat() + 'Z',
-        'close': datetime.utcnow().isoformat() + 'Z',
-        'totalRewardPoint': 10000,
-        'tokens': [{"name": "hoge", "description": "", "thumbnailUrl": ""}],
-        'prizes': [],
+        "title": "hoge",
+        "description": "hoge",
+        "organizerId": "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
+        "lmsrB": 0,
+        "open": datetime.utcnow().isoformat() + "Z",
+        "close": datetime.utcnow().isoformat() + "Z",
+        "totalRewardPoint": 10000,
+        "tokens": [{
+            "name": "hoge",
+            "description": "",
+            "thumbnailUrl": ""
+        }],
+        "prizes": [],
     }
-    res = requests.post(url("/markets/"), json.dumps(invalid_payload), headers=headers)
+    res = requests.post(url("/markets/"),
+                        json.dumps(invalid_payload),
+                        headers=headers)
     assert_eq(res.status_code, 400)
 
     # 正しいペイロードでマーケットが作成できることをテストする
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     valid_payload = {
-        'title': "hoge",
-        'description': "",
-        'organizerId': "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
-        'lmsrB': 0,
-        'open': datetime.utcnow().isoformat() + 'Z',
-        'close': datetime.utcnow().isoformat() + 'Z',
-        'totalRewardPoint': 10000,
-        'tokens': [{"name": "hoge", "description": "", "thumbnailUrl": ""}],
-        'prizes': [{"name": "hoge", "target": "everyone", "thumbnailUrl": ""}],
+        "title": "hoge",
+        "description": "",
+        "organizerId": "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
+        "lmsrB": 0,
+        "open": datetime.utcnow().isoformat() + "Z",
+        "close": datetime.utcnow().isoformat() + "Z",
+        "totalRewardPoint": 10000,
+        "tokens": [{
+            "name": "hoge",
+            "description": "",
+            "thumbnailUrl": ""
+        }],
+        "prizes": [{
+            "name": "hoge",
+            "target": "everyone",
+            "thumbnailUrl": ""
+        }],
     }
-    res = requests.post(url("/markets/"), json.dumps(valid_payload), headers=headers)
+    res = requests.post(url("/markets/"),
+                        json.dumps(valid_payload),
+                        headers=headers)
     assert_eq(res.status_code, 201)
     assert_eq(type(res.json()["id"]) is str, True)
     market_id = res.json()["id"]
@@ -256,30 +301,41 @@ def test_markets_api():
     assert_eq(res.json()["lmsrB"], valid_payload["lmsrB"])
     assert_eq(res.json()["open"], valid_payload["open"])
     assert_eq(res.json()["close"], valid_payload["close"])
-    assert_eq(res.json()["totalRewardPoint"], valid_payload["totalRewardPoint"])
+    assert_eq(res.json()["totalRewardPoint"],
+              valid_payload["totalRewardPoint"])
     assert_eq(res.json()["tokens"], valid_payload["tokens"])
     assert_eq(len(res.json()["prizes"]), 1)
-    assert_eq(res.json()["prizes"][0]["name"], valid_payload["prizes"][0]["name"])
-    assert_eq(res.json()["prizes"][0]["target"], valid_payload["prizes"][0]["target"])
-    assert_eq(res.json()["prizes"][0]["thumbnailUrl"], valid_payload["prizes"][0]["thumbnailUrl"])
+    assert_eq(res.json()["prizes"][0]["name"],
+              valid_payload["prizes"][0]["name"])
+    assert_eq(res.json()["prizes"][0]["target"],
+              valid_payload["prizes"][0]["target"])
+    assert_eq(
+        res.json()["prizes"][0]["thumbnailUrl"],
+        valid_payload["prizes"][0]["thumbnailUrl"],
+    )
 
 
 ## Utils
 def url(path):
     return f"http://localhost:8081{path}"
 
+
 def empty_headers():
     return {}
 
+
 def content_type_json(headers):
-    headers['Content-Type'] = 'application/json'
+    headers["Content-Type"] = "application/json"
     return headers
 
+
 def bearer_token(headers, token):
-    headers['Authorization'] = f"Bearer: {token}"
+    headers["Authorization"] = f"Bearer: {token}"
     return headers
+
 
 def assert_eq(found, expected):
     assert found == expected, f"expected [{expected}] but found [{found}]"
+
 
 main()
--- backend/tests/scenarios/scenario_market_reward.py
+++ backend/tests/scenarios/scenario_market_reward.py
@@ -6,6 +6,7 @@ import time
 AdminAccessToken = "test_admin_access_token"
 UserAccessToken = "test_user_access_token"
 
+
 def main():
     create_user()
 
@@ -37,6 +38,7 @@ def main():
 
     print("[ OK ]")
 
+
 def create_user():
     headers = content_type_json(bearer_token({}, UserAccessToken))
     payload = {
@@ -46,9 +48,11 @@ def create_user():
     res = requests.post(url("/users/"), json.dumps(payload), headers=headers)
     assert_eq(res.status_code, 201)
 
+
 # 報酬を作成する
 def create_prize():
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     payload = {
         "name": "The prize",
         "description": "",
@@ -60,31 +64,41 @@ def create_prize():
     assert_eq(res.status_code, 201)
     return res.json()
 
+
 # マーケットを作成する
 def create_market():
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     payload = {
         'title': "The market",
         'description': "",
         'organizerId': "ec2966c5-d661-4a9b-b377-9e00f21d7dd4",
         'lmsrB': 100,
         'open': datetime.utcnow().isoformat() + 'Z',
-        'close': (datetime.utcnow() + timedelta(seconds=5) ).isoformat() + 'Z',
+        'close': (datetime.utcnow() + timedelta(seconds=5)).isoformat() + 'Z',
         'totalRewardPoint': 10000,
-        'tokens': [{"name": "hoge", "description": "", "thumbnailUrl": ""}],
-        'prizes': [{"name": "hoge", "target": "everyone", "thumbnailUrl": ""}],
+        'tokens': [{
+            "name": "hoge",
+            "description": "",
+            "thumbnailUrl": ""
+        }],
+        'prizes': [{
+            "name": "hoge",
+            "target": "everyone",
+            "thumbnailUrl": ""
+        }],
     }
     res = requests.post(url("/markets/"), json.dumps(payload), headers=headers)
     assert_eq(res.status_code, 201)
     return res.json()
 
+
 def check_markets():
-    headers = {
-        "X-Appengine-Cron": "True"
-    }
+    headers = {"X-Appengine-Cron": "True"}
     res = requests.get(url("/cronjob/check_markets/"), headers=headers)
     assert_eq(res.status_code, 200)
     return res.json()
+
 
 def join_market(market_id):
     headers = content_type_json(bearer_token(empty_headers(), UserAccessToken))
@@ -94,17 +108,24 @@ def join_market(market_id):
         "time": datetime.utcnow().isoformat() + 'Z',
         "type": "coinSupply",
     }
-    res = requests.post(url(f"/markets/{market_id}/orders/"), json.dumps(payload), headers= headers)
+    res = requests.post(url(f"/markets/{market_id}/orders/"),
+                        json.dumps(payload),
+                        headers=headers)
     assert_eq(res.status_code, 201)
 
+
 def resolve_market(market_id):
-    headers = content_type_json(bearer_token(empty_headers(), AdminAccessToken))
+    headers = content_type_json(bearer_token(empty_headers(),
+                                             AdminAccessToken))
     payload = {
         "status": "resolved",
         "resolvedTokenName": "hoge",
     }
-    res = requests.put(url(f"/markets/{market_id}/"), json.dumps(payload), headers=headers)
+    res = requests.put(url(f"/markets/{market_id}/"),
+                       json.dumps(payload),
+                       headers=headers)
     assert_eq(res.status_code, 201)
+
 
 def get_my_info():
     headers = content_type_json(bearer_token(empty_headers(), UserAccessToken))
@@ -112,12 +133,15 @@ def get_my_info():
     assert_eq(res.status_code, 200)
     return res.json()
 
+
 def request_prize_trade(prize_id):
     headers = content_type_json(bearer_token(empty_headers(), UserAccessToken))
     payload = {
         "prizeId": prize_id,
     }
-    res = requests.post(url("/users/me/prize_trade_history/"), json.dumps(payload), headers=headers)
+    res = requests.post(url("/users/me/prize_trade_history/"),
+                        json.dumps(payload),
+                        headers=headers)
     assert_eq(res.status_code, 201)
 
 
@@ -125,18 +149,23 @@ def request_prize_trade(prize_id):
 def url(path):
     return f"http://localhost:8081{path}"
 
+
 def empty_headers():
     return {}
+
 
 def content_type_json(headers):
     headers['Content-Type'] = 'application/json'
     return headers
 
+
 def bearer_token(headers, token):
     headers['Authorization'] = f"Bearer: {token}"
     return headers
 
+
 def assert_eq(found, expected):
     assert found == expected, f"expected [{expected}] but found [{found}]"
+
 
 main()
