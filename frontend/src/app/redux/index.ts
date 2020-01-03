29c645b68277715e2f6bfea82511c85e58b1fd0f
AtsukiTak <takatomgoo@gmail.com>
2020-01-04 01:41:48 +0900

    remove prize model from frontend

--- frontend/src/app/redux/index.ts
+++ frontend/src/app/redux/index.ts
@@ -1,14 +1,12 @@
 import { combineReducers, Store as ReduxStore } from "redux";
 
-import * as prize from "./prize";
 import * as user from "./user";
 
-export type RootState = { prize: prize.State; user: user.State };
+export type RootState = { user: user.State };
 
-export type RootAction = prize.Action | user.Action;
+export type RootAction = user.Action;
 
 export const rootReducer = combineReducers<RootState, RootAction>({
-  prize: prize.reducer,
   user: user.reducer
 });
 
