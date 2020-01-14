9254ad033a8ef0083c6e447329303e005c61a75f
AtsukiTak <takatomgoo@gmail.com>
2020-01-15 01:12:39 +0900

    move domain::num to domain::num::point

--- backend/src/domain/num.rs
+++ backend/src/domain/num.rs
@@ -1,13 +1,1 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-#[derive(Add, Sub, Neg, AddAssign, From)]
-pub struct Point(i32);
-
-impl Point {
-    pub const fn zero() -> Point {
-        Point(0)
-    }
-
-    pub fn as_i32(&self) -> i32 {
-        self.0
-    }
-}
+pub mod point;
