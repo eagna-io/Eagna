e3e35e5b6d511c72d5dfeae218e5da7aea63add3
AtsukiTak <takatomgoo@gmail.com>
2020-02-19 19:50:12 +0900

    add Market::new_order

--- backend/crop_domain/src/market/model.rs
+++ backend/crop_domain/src/market/model.rs
@@ -5,7 +5,7 @@ use crate::market::order::model::Order;
 use std::collections::HashMap;
 use uuid::Uuid;
 
-use num::ShareNum;
+use num::{ShareNum, TipNum};
 
 pub struct Market {
     pub id: MarketId,
@@ -18,8 +18,24 @@ pub struct Market {
 pub struct MarketId(Uuid);
 
 impl Market {
+    /// 対象のOutcomeを1つ購入する
     pub fn new_order(&mut self, outcome: OutcomeId, account: AccountId) -> Order {
+        let tip_cost = self.compute_cost(outcome);
+        let order = Order::new(outcome, account, tip_cost);
+        // Orderを記録する
+        self.orders.push(order);
+        self.increment_share(outcome);
+        order
+    }
+
+    /// 対象のOutcomeを1つ購入するときのTipのコストを計算する
+    pub fn compute_cost(&self, outcome: OutcomeId) -> TipNum {
         todo!();
+    }
+
+    /// 対象のOutcomeのShareを1つ追加する
+    fn increment_share(&mut self, outcome: OutcomeId) {
+        *self.share_distri.get_mut(&outcome).unwrap() += ShareNum::ONE;
     }
 }
 
--- backend/crop_domain/src/market/model/num.rs
+++ backend/crop_domain/src/market/model/num.rs
@@ -1,5 +1,11 @@
+use derive_more::{Add, AddAssign};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct TipNum(isize);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, AddAssign, Add)]
 pub struct ShareNum(isize);
+
+impl ShareNum {
+    pub const ONE: ShareNum = ShareNum(1);
+}
