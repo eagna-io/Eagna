c49f95dcf978110a513b76c5c719d318bcd26b78
AtsukiTak <takatomgoo@gmail.com>
2020-02-19 20:47:17 +0900

    add PriceComputer

--- backend/crop_domain/src/market/model.rs
+++ backend/crop_domain/src/market/model.rs
@@ -1,41 +1,41 @@
 pub mod num;
 
 use crate::account::model::AccountId;
-use crate::market::order::model::Order;
+use crate::market::{order::model::Order, service::PriceComputer};
 use std::collections::HashMap;
 use uuid::Uuid;
 
-use num::{ShareNum, TipNum};
+use num::ShareNum;
 
 pub struct Market {
     pub id: MarketId,
     pub orders: Vec<Order>,
 
     share_distri: HashMap<OutcomeId, ShareNum>,
+    price_computer: PriceComputer,
 }
 
 impl Market {
+    /// 新しくMarketを作成する
     pub fn new() -> Market {
         Market {
             id: MarketId::new(),
             orders: Vec::new(),
             share_distri: HashMap::new(),
+            price_computer: PriceComputer::default(),
         }
     }
 
     /// 対象のOutcomeを1つ購入する
     pub fn new_order(&mut self, outcome: OutcomeId, account: AccountId) -> Order {
-        let tip_cost = self.compute_cost(outcome);
+        let tip_cost = self
+            .price_computer
+            .compute_price(&self.share_distri, outcome);
         let order = Order::new(outcome, account, tip_cost);
         // Orderを記録する
         self.orders.push(order);
         self.increment_share(outcome);
         order
-    }
-
-    /// 対象のOutcomeを1つ購入するときのTipのコストを計算する
-    pub fn compute_cost(&self, outcome: OutcomeId) -> TipNum {
-        todo!();
     }
 
     /// 対象のOutcomeのShareを1つ追加する
--- backend/crop_domain/src/market/model/num.rs
+++ backend/crop_domain/src/market/model/num.rs
@@ -1,11 +1,21 @@
 use derive_more::{Add, AddAssign};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct TipNum(isize);
+pub struct TipNum(pub(in crate::market) i32);
+
+impl TipNum {
+    pub fn as_i32(&self) -> i32 {
+        self.0
+    }
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, AddAssign, Add)]
-pub struct ShareNum(isize);
+pub struct ShareNum(pub(in crate::market) i32);
 
 impl ShareNum {
     pub const ONE: ShareNum = ShareNum(1);
+
+    pub fn as_i32(&self) -> i32 {
+        self.0
+    }
 }
