0943763c5c1b4e6b9e09bca5e919e1e309ddd272
hayatasatomi <lionvibe.14@gmail.com>
2020-03-06 19:11:02 +0900

    add color.ts

---
+++ frontend/src/app/components/color.ts
@@ -0,0 +1,37 @@
+export class Color {
+  readonly hex: string;
+
+  constructor(
+    readonly red: number,
+    readonly green: number,
+    readonly blue: number
+  ) {
+    this.hex = `#${toHex(this.red)}${toHex(this.green)}${toHex(this.blue)}`;
+  }
+
+  static fromHex(hex: string): Color {
+    const red = fromHex(hex.slice(1, 3));
+    const green = fromHex(hex.slice(3, 5));
+    const blue = fromHex(hex.slice(5, 7));
+    return new Color(red, green, blue);
+  }
+}
+
+function toHex(i: number): string {
+  return ("0" + i.toString(16)).slice(-2);
+}
+
+function fromHex(hex: string): number {
+  return parseInt(hex, 16);
+}
+
+export const BackgroundMainColor = Color.fromHex("#242423");
+
+export const PurpleColor = Color.fromHex("#BB86FC");
+export const RankingColor = Color.fromHex("#FAD160");
+export const FontBaseColor = Color.fromHex("#AEAEAE");
+export const RedDisagreeColor = Color.fromHex("#F74C61");
+export const GreenAgreeColor = Color.fromHex("#39CCBE");
+export const MarketPredictionValueColor = Color.fromHex("#9B9A9A");
+
+export const EagnaColor = Color.fromHex("#5BB192");
