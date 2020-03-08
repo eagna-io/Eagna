f539f2ae7c537ae69b792269be0d5a045ac1c292
AtsukiTak <takatomgoo@gmail.com>
2020-03-08 16:37:50 +0900

    vote rpc filterを少しだけ実装

--- backend/crop_server/src/routes/rpc.rs
+++ backend/crop_server/src/routes/rpc.rs
@@ -2,6 +2,11 @@ mod vote;
 
 use warp::{filters::path, reject::Rejection, reply::Reply, Filter};
 
+/// ## JSON RPC
+///
+/// ### Path
+///
+/// /rpc
 pub fn filter() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Copy {
-    path::path("rpc").and(self::vote::filter())
+    path::path("rpc").and(path::end()).and(self::vote::filter())
 }
--- backend/crop_server/src/routes/rpc/vote.rs
+++ backend/crop_server/src/routes/rpc/vote.rs
@@ -1,9 +1,12 @@
-use serde::Deserialize;
+use crate::state;
+use crop_domain::market::model::MarketId;
+use futures::future::FutureExt as _;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use warp::{reject::Rejection, reply::Reply, Filter};
 use warp_json_rpc::{
     filters::{json_rpc, method, params},
-    Builder,
+    Builder, Error,
 };
 
 #[derive(Debug, Deserialize)]
@@ -11,9 +14,26 @@ struct Params {
     market_id: Uuid,
 }
 
+#[derive(Debug, Serialize)]
+struct Success();
+
+#[derive(Debug, Serialize)]
+struct Failure();
+
 pub fn filter() -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Copy {
     json_rpc()
         .and(method("vote"))
         .and(params::<Params>())
-        .map(|res: Builder, params: Params| res.success("HOGE").unwrap())
+        .and_then(|res: Builder, params: Params| {
+            handler(params).map(|r| Ok::<_, Rejection>(res.result(r).unwrap()))
+        })
+}
+
+async fn handler(params: Params) -> Result<Success, Error> {
+    let market_id = MarketId(params.market_id);
+    if let Some(market) = state::get_market_state(market_id).await {
+        todo!()
+    } else {
+        todo!()
+    }
 }
