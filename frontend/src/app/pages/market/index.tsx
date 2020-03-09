4d0b1ae5b6ac2dfe10add8bf795bb070b739806b
hayatasatomi <lionvibe.14@gmail.com>
2020-03-09 16:33:41 +0900

    add Guide

--- frontend/src/app/pages/market/index.tsx
+++ frontend/src/app/pages/market/index.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { BackgroundMainColor, PurpleColor, TextBaseColor, RankingColor } from "app/components/color";
 
 import { RootState } from "app/redux";
-import { actions } from "app/redux/chart";
+import { actions, Data } from "app/redux/chart";
 import { now } from "model/time";
 
 import Header from "./components/organisms/header";
@@ -13,7 +13,10 @@ import Feed from "./components/organisms/feed";
 
 const Page: React.FC = () => {
 
-  const records = useSelector((state: RootState) => state.chart.records);
+  const [datasets, records] = useSelector((state: RootState) => [
+    state.chart.datasets,
+    state.chart.records
+  ]);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
@@ -29,6 +32,8 @@ const Page: React.FC = () => {
     };
   }, [dispatch]);
 
+  const publicPred = getPublicPrediction(datasets.win);
+
   return (
     <Container>
       <ChartContainer />
@@ -43,15 +48,31 @@ const Page: React.FC = () => {
         </Ranking>
         <Feed records={records} />
       </SubContainer>
+      <Guide>
+        <PredictionTheme>{predictionTheme}</PredictionTheme>
+        <PublicPrediction>
+          みんなの予想
+          <PredictionValue>{publicPred}<Sm>%</Sm></PredictionValue>
+        </PublicPrediction>
+        </Guide>
     </Container>
   );
 };
 
 export default Page;
 
+const getPublicPrediction = (data: Data[]): string => {
+  if (data.length === 0) {
+    return "-";
+  } else {
+    return Math.floor(data[data.length - 1][1] / 10).toString();
+  }
+};
+
 const marketTitle = 'RAGE Shadowverse 2020 Spring'
 const ranking = 2
 const paticipantsNum = 358
+const predictionTheme = 'GRAND FINALS Shimon/REVが優勝する確率を予想せよ'
 
 const botNames = [
   "ふるさわゆうや",
@@ -97,4 +118,37 @@ const RankNum = styled.span`
   color: ${RankingColor.hex};
   font-weight: 800;
   margin-right: 4px;
+`;
+
+const Guide = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 20px 0px 20px;
+`;
+
+const PredictionTheme = styled.div`
+  color: ${TextBaseColor.hex}
+  font-size: 14px;
+  whiteSpace: 'pre-line';
+  width: 70%;
+`;
+
+const PublicPrediction = styled.div`
+  color: ${TextBaseColor.hex}
+  text-align: center;
+  font-size: 14px;
+  font-weight: 600;
+  width: 30%;
+`;
+
+const PredictionValue = styled.div`
+  font-size: 56px;
+  line-height: 56px;
+  font-weight: 800;
+`;
+
+const Sm = styled.span`
+  font-size: 20px;
+  font-weight: 800;
 `;
