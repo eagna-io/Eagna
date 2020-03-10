6331720d7bde2b36cc320c2241c336c9b19597b6
AtsukiTak <takatomgoo@gmail.com>
2020-03-10 22:12:19 +0900

    getmarketinfo rpcのclientの追加

--- frontend/src/infra/rpc/vote.ts
+++ frontend/src/infra/rpc/vote.ts
@@ -1,20 +1,12 @@
-import { RPC_URL } from "../rpc";
+import * as D from "@mojotech/json-type-validation";
+
+import { call } from "../rpc";
 
 export interface Params {
-  marketId: string,
-  outcomeId: string,
-  accountName: string,
+  marketId: string;
+  outcomeId: string;
+  accountName: string;
 }
 
-export const vote = (params: Params): Promise<void> => {
-  console.log("vote rpc");
-  return fetch(RPC_URL, {
-    method: "POST",
-    body: JSON.stringify(params),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(res => res.json())
-    .then(json => console.log(json))
-};
+export const vote = (params: Params): Promise<void> =>
+  call("vote", params, D.succeed(undefined));
