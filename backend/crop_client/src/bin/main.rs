9b4f73af3a8505244974f2e3f01aa624d6dd40b6
AtsukiTak <takatomgoo@gmail.com>
2020-03-10 12:31:03 +0900

    remove unused files

--- backend/crop_client/src/bin/main.rs
+++
@@ -1,6 +0,0 @@
-use crop_client::connect;
-
-#[tokio::main]
-async fn main() {
-    connect("ws://127.0.0.1:3030/stream").await;
-}
--- backend/tests/run-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-for s in $(ls scenarios); do
-  ./run.sh scenarios/$s
-done
