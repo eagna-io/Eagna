683561dc210b46e11d8b25075ba972af541794a2
AtsukiTak <takatomgoo@gmail.com>
2020-03-10 19:09:05 +0900

    fix server

--- backend/crop_server/src/bin/main.rs
+++ backend/crop_server/src/bin/main.rs
@@ -1,13 +1,10 @@
-use crop_domain::market::model::{Market, OutcomeId};
+use crop_domain::market::model::Market;
 use crop_server::{context::Context, server};
-use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
     let context = Context::new();
-    let outcome1_id = OutcomeId(Uuid::parse_str("4ef1a32161bd4c5684c1ddb327d38b91").unwrap());
-    let outcome2_id = OutcomeId(Uuid::parse_str("a490323fc5444ffbbc093f18496c3e1f").unwrap());
-    let market = Market::new(&[outcome1_id, outcome2_id]);
+    let market = Market::new("テストマーケット".into(), &["win".into(), "lose".into()]);
 
     println!("Market id : {}", market.id.0);
 
