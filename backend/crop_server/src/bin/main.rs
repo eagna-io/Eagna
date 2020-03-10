4e8f61da7cfd4a55a73ea5655d8338ef176995e3
AtsukiTak <takatomgoo@gmail.com>
2020-03-10 15:34:07 +0900

    print market_id on start up

--- backend/crop_server/src/bin/main.rs
+++ backend/crop_server/src/bin/main.rs
@@ -7,9 +7,11 @@ async fn main() {
     let context = Context::new();
     let outcome1_id = OutcomeId(Uuid::parse_str("4ef1a32161bd4c5684c1ddb327d38b91").unwrap());
     let outcome2_id = OutcomeId(Uuid::parse_str("a490323fc5444ffbbc093f18496c3e1f").unwrap());
-    context
-        .add_new_market(Market::new(&[outcome1_id, outcome2_id]))
-        .await;
+    let market = Market::new(&[outcome1_id, outcome2_id]);
+
+    println!("Market id : {}", market.id.0);
+
+    context.add_new_market(market).await;
 
     server::Server::bind(([127, 0, 0, 1], 3030), context).await;
 }
