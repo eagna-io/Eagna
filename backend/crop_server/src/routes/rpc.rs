b2ba6b4f570ad51077905c41bacd06fc77af3f81
AtsukiTak <takatomgoo@gmail.com>
2020-03-12 22:42:40 +0900

    add CORS allow header

--- backend/crop_server/src/routes/rpc.rs
+++ backend/crop_server/src/routes/rpc.rs
@@ -15,7 +15,10 @@ use warp::{
 ///
 /// /rpc
 pub fn filter(ctx: Context) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-    let cors = cors::cors().allow_any_origin().allow_methods(vec!["POST"]);
+    let cors = cors::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["POST", "OPTIONS"])
+        .allow_header("Content-Type");
     path::path("rpc")
         .and(path::end())
         .and(self::vote::filter(ctx.clone()).or(self::get_market_info::filter(ctx)))
