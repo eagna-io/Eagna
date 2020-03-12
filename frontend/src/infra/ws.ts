75ed94f0cda49d8aed1c333260bdf1afd405f1a6
AtsukiTak <takatomgoo@gmail.com>
2020-03-12 13:57:13 +0900

    fix websocket FeedMsg format

--- frontend/src/infra/ws.ts
+++ frontend/src/infra/ws.ts
@@ -1,3 +1,5 @@
+import * as D from "@mojotech/json-type-validation";
+
 export const WS_URL = process.env.REACT_APP_API_BASE + "/ws";
 
 export interface Params {
@@ -9,15 +11,26 @@ export const open = ({ marketId, onFeedMsg }: Params) => {
   const ws = new WebSocket(`${WS_URL}/${marketId}`);
   ws.onmessage = event => {
     const data = JSON.parse(event.data);
-    console.log(event);
-
-    // TODO
-    onFeedMsg(data as FeedMsg);
+    const decoded = FeedMsgDecoder.run(data);
+    if (decoded.ok) {
+      onFeedMsg(decoded.result);
+    } else {
+      console.error(decoded);
+    }
   };
 };
 
 export interface FeedMsg {
-  outcomeId: string;
+  outcome: "realize" | "unrealize";
   accountName: string;
   timestamp: number;
 }
+
+const FeedMsgDecoder: D.Decoder<FeedMsg> = D.object({
+  outcome: D.union(
+    D.constant<"realize">("realize"),
+    D.constant<"unrealize">("unrealize")
+  ),
+  accountName: D.string(),
+  timestamp: D.number()
+});
