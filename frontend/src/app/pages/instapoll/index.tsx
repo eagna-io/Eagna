06973982ef693dfe707b7fa7b8bac5f347a684fa
hayatasatomi <lionvibe.14@gmail.com>
2020-03-16 13:33:40 +0900

    add timer

--- frontend/src/app/pages/instapoll/index.tsx
+++ frontend/src/app/pages/instapoll/index.tsx
@@ -1,17 +1,46 @@
 import React from "react";
 import styled from "styled-components";
 
-import { BackgroundMainColor } from "app/components/color";
+import {
+  BackgroundMainColor,
+  ShadowGray,
+  WhiteBaseColor
+} from "app/components/color";
 
 export const InstapollPage: React.FC = () => {
   return (
-    <Container>x</Container>
+    <Container>
+      <TimerComponent>
+        <Timer>2:57</Timer>
+      </TimerComponent>
+    </Container>
   );
 }
 
 const Container = styled.div`
   width: 100vw;
   height: calc(100vh - 75px);
+  padding: 8px 20px;
   background-color: ${BackgroundMainColor.hex};
   user-select: none;
+`;
+
+const TimerComponent = styled.div`
+  width: 16vw;
+  height: 16vw;
+  margin 0px auto;
+  border-radius: 50%;
+  border: solid 3px ${ShadowGray.hex};
+  position: relative
+`;
+
+const Timer = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  -webkit- transform: translateY(-50%) translateX(-50%);
+  font-size: 16px;
+  font-weight: 800;
+  color: ${WhiteBaseColor.hex}
 `;
