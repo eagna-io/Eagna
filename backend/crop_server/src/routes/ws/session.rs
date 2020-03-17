76bfae53495a7cb46104414ee9d4dafd9e9a6d49
AtsukiTak <takatomgoo@gmail.com>
2020-03-17 16:05:51 +0900

    ws::Sessionの内部表現を修正

--- backend/crop_server/src/routes/ws/session.rs
+++ backend/crop_server/src/routes/ws/session.rs
@@ -6,51 +6,68 @@ use futures::{
     sink::{Sink, SinkExt as _},
     stream::{Stream, StreamExt as _, TryStreamExt as _},
 };
-use std::convert::TryFrom;
+use std::{convert::TryFrom, pin::Pin};
 use tokio::sync::broadcast::Receiver;
-use warp::filters::ws::{Message, WebSocket};
+use warp::filters::ws::WebSocket;
 
 pub struct Session {
     contest: ContestManager,
-    ws: WebSocket,
+    ws_stream: Pin<Box<dyn Stream<Item = anyhow::Result<IncomingMsg>> + Send + 'static>>,
+    ws_sink: Pin<Box<dyn Sink<OutgoingMsg, Error = anyhow::Error> + Send + 'static>>,
 }
 
 impl Session {
     pub fn new(contest: ContestManager, ws: WebSocket) -> Session {
-        Session { contest, ws }
+        let (sink, stream) = ws.split();
+
+        let ws_stream = stream
+            .err_into::<anyhow::Error>()
+            .and_then(|msg| future::ready(IncomingMsg::try_from(msg)))
+            .boxed();
+
+        let ws_sink = Box::pin(
+            sink.sink_err_into::<anyhow::Error>()
+                .with(|msg: OutgoingMsg| future::ok(msg.into())),
+        );
+
+        Session {
+            contest,
+            ws_stream,
+            ws_sink,
+        }
     }
 
     pub async fn handle(self) {
-        let Session { contest, ws } = self;
+        let Session {
+            contest,
+            ws_stream,
+            ws_sink,
+        } = self;
         let subscriber = contest.subscribe();
 
-        let (sink, stream) = ws.split();
-        let fut1 = handle_outgoing(sink, subscriber);
-        let fut2 = handle_incoming(stream, contest);
+        let fut1 = handle_outgoing(ws_sink, subscriber);
+        let fut2 = handle_incoming(ws_stream, contest);
         future::join(fut1, fut2).await;
     }
 }
 
 async fn handle_outgoing(
-    sink: impl Sink<Message, Error = warp::Error> + Unpin,
+    mut sink: impl Sink<OutgoingMsg, Error = anyhow::Error> + Unpin,
     subscriber: Receiver<Comment>,
 ) {
     let mut msg_stream = subscriber
         .err_into::<anyhow::Error>()
-        .map_ok(|comment| OutgoingMsg::Comment(comment).into());
-    sink.sink_err_into::<anyhow::Error>()
-        .send_all(&mut msg_stream)
+        .map_ok(|comment| OutgoingMsg::Comment(comment));
+    sink.send_all(&mut msg_stream)
         .await
         .unwrap_or_else(|e| log::debug!("{:?}", e))
 }
 
 async fn handle_incoming(
-    stream: impl Stream<Item = Result<Message, warp::Error>> + Unpin,
+    stream: impl Stream<Item = anyhow::Result<IncomingMsg>> + Unpin,
     contest: ContestManager,
 ) {
     if let Err(e) = stream
-        .err_into::<anyhow::Error>()
-        .and_then(|msg| future::ready(IncomingMsg::try_from(msg)))
         .try_for_each(move |msg| handle_each_incoming_msg(msg, contest.clone()))
         .await
     {
