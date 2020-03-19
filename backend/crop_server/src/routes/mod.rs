87b45c4f74ff33bfcb45f084e493a6a285581f70
AtsukiTak <takatomgoo@gmail.com>
2020-03-19 09:23:21 +0900

    add CORS setting

--- backend/crop_server/src/routes/mod.rs
+++ backend/crop_server/src/routes/mod.rs
@@ -2,8 +2,17 @@ pub mod contest;
 pub mod ws;
 
 use crate::context::Context;
-use warp::{reject::Rejection, reply::Reply, Filter};
+use warp::{filters::cors, reject::Rejection, reply::Reply, Filter};
 
 pub fn filter(ctx: Context) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-    self::contest::poll::post::filter(ctx.clone()).or(self::ws::filter(ctx))
+    let cors_wrapper = cors::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["POST", "OPTIONS"])
+        .allow_header("Content-Type");
+
+    let rest = self::contest::poll::post::filter(ctx.clone());
+
+    let rest_with_cors = rest.with(cors_wrapper);
+
+    self::ws::filter(ctx).or(rest_with_cors)
 }
