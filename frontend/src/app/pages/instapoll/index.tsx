4685bfc09d4d81c3d79e02e425a60fd5405096c9
AtsukiTak <takatomgoo@gmail.com>
2020-04-07 13:50:11 +0900

    Pageコンポーネントを切り出し

--- frontend/src/app/pages/instapoll/index.tsx
+++ frontend/src/app/pages/instapoll/index.tsx
@@ -12,10 +12,7 @@ import {
 } from "app/components/color";
 import * as websocket from "infra/ws/contest";
 
-import { Timer } from "./components/organisms/timer";
-import { CommentCard } from "./components/organisms/commentCard";
-import { ChoiceList } from "./components/organisms/choiceList";
-import { ReactComponent as SubmitIcon } from "./components/atoms/images/send.svg";
+import { Page, LoadingPage } from "./page";
 import { reducer, initialState } from "./reducer";
 
 export const InstapollPage: React.FC = () => {
@@ -58,113 +55,17 @@ export const InstapollPage: React.FC = () => {
     };
   }, []);
 
-  if (poll && timer !== undefined) {
+  if (poll !== undefined && timer !== undefined && ws !== undefined) {
     return (
-      <Container>
-        <Timer content={timer} />
-        <CommentFeed>
-          {comments.map(comment => (
-            <CommentCard comment={comment} />
-          ))}
-        </CommentFeed>
-        <PollCard>
-          <Theme>{poll.title}</Theme>
-          <ChoiceList
-            poll={poll}
-            selected={selected}
-            onSelected={choice => {
-              if (ws) {
-                ws.send(
-                  JSON.stringify({
-                    type: "updateChoice",
-                    account,
-                    choice
-                  })
-                );
-                setSelected(choice);
-              }
-            }}
-          />
-          <CommentContainer>
-            <CommentInput
-              type="text"
-              placeholder="コメントする"
-              value={commentInput}
-              onChange={e => setCommentInput(e.target.value)}
-            />
-            <Submit
-              onClick={() => {
-                if (commentInput && ws) {
-                  ws.send(
-                    JSON.stringify({
-                      type: "addComment",
-                      account,
-                      comment: commentInput
-                    })
-                  );
-                  setCommentInput("");
-                }
-              }}
-            />
-          </CommentContainer>
-        </PollCard>
-      </Container>
+      <Page
+        account={account}
+        poll={poll}
+        comments={comments}
+        timer={timer}
+        ws={ws}
+      />
     );
   } else {
-    return <Container>Loading...</Container>;
+    return <LoadingPage />;
   }
 };
-
-const Container = styled.div`
-  width: 100vw;
-  height: calc(100vh - 75px);
-  padding: 8px 28px;
-  background-color: ${BackgroundMainColor.hex};
-  user-select: none;
-  position: relative;
-`;
-
-const CommentFeed = styled.div`
-  width: 100%;
-  height: 50%;
-`;
-
-const PollCard = styled.div`
-  position: absolute;
-  height: 244px;
-  bottom: 20px;
-  width: calc(100vw - 56px);
-  border-radius: 4px;
-  padding: 8px 13px;
-  background-color: ${WhiteBaseColor.hex};
-  box-shadow: 0 2px 4px 0 ${BlackColor.rgba(0.5)};
-`;
-
-const Theme = styled.div`
-  width: 100%;
-  font-size: 16px;
-  margin-bottom: 24px;
-`;
-
-const CommentContainer = styled.div`
-  width: 100%;
-  display: flex;
-`;
-
-const CommentInput = styled.input`
-  width: 86%;
-  border-radius: 24px;
-  height: 30px;
-  border: solid 1px ${TextBaseColor.hex};
-  margin-right: 8px;
-  padding: 0px 16px;
-  font-size: 16px;
-  line-height: 30px;
-  transform: scale(0.95);
-`;
-
-const Submit = styled(SubmitIcon)`
-  width: 32px;
-  height: 28px;
-  cursor: pointer;
-`;
