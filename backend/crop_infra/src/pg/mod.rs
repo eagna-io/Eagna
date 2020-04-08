2e07027e49ed3e4e7b7bb241f40316229a08961a
AtsukiTak <takatomgoo@gmail.com>
2020-04-08 16:48:01 +0900

    minimal crop_infra

---
+++ backend/crop_infra/src/pg/mod.rs
@@ -0,0 +1,30 @@
+pub(crate) mod schema;
+
+use diesel::{
+    pg::PgConnection,
+    r2d2::{ConnectionManager, Pool as PgPool, PooledConnection},
+};
+
+pub type Connection = PooledConnection<ConnectionManager<PgConnection>>;
+
+#[derive(Clone)]
+pub struct Pool {
+    pool: PgPool<ConnectionManager<PgConnection>>,
+}
+
+impl Pool {
+    pub fn new(url: impl Into<String>) -> Pool {
+        let manager = ConnectionManager::<PgConnection>::new(url);
+        let pool = PgPool::new(manager).expect("Failed to create pg connection pool");
+        Pool { pool }
+    }
+
+    pub async fn with_conn<F, T>(&self, func: F) -> anyhow::Result<T>
+    where
+        F: FnOnce(Connection) -> T + Send + 'static,
+        T: Send + 'static,
+    {
+        let pool = self.pool.clone();
+        tokio::task::spawn_blocking(move || Ok(func(pool.get()?))).await?
+    }
+}
