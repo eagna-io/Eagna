4761e1c2c5a7cc716909fd3a98b39703c8310c20
AtsukiTak <takatomgoo@gmail.com>
2020-04-11 19:42:42 +0900

    fix PollTable

--- backend/crop_infra/src/pg/poll.rs
+++ backend/crop_infra/src/pg/poll.rs
@@ -6,7 +6,6 @@ use chrono::{DateTime, Utc};
 use diesel::{
     expression_methods::{NullableExpressionMethods as _, PgExpressionMethods as _},
     prelude::*,
-    result::Error as PgError,
 };
 use uuid::Uuid;
 
@@ -20,13 +19,13 @@ pub trait PollTable {
         Ok(())
     }
 
-    fn query_by_id(&self, id: &Uuid) -> anyhow::Result<Option<QueriedPoll>> {
-        match polls::table
+    fn query_by_contest_id(&self, id: &Uuid) -> anyhow::Result<Vec<QueriedPoll>> {
+        Ok(polls::table
             .left_join(
                 choices::table
                     .on(polls::resolved_choice_id.is_not_distinct_from(choices::id.nullable())),
             )
-            .filter(polls::id.eq(id))
+            .filter(polls::contest_id.eq(id))
             .select((
                 polls::id,
                 polls::contest_id,
@@ -38,12 +37,7 @@ pub trait PollTable {
                 choices::color.nullable(),
                 choices::idx.nullable(),
             ))
-            .first::<QueriedPoll>(self.conn())
-        {
-            Ok(poll) => Ok(Some(poll)),
-            Err(PgError::NotFound) => Ok(None),
-            Err(e) => Err(e.into()),
-        }
+            .load::<QueriedPoll>(self.conn())?)
     }
 }
 
