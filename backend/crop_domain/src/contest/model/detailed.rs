5202e9aff9be74036d4f19f99949b24ac87b048b
AtsukiTak <takatomgoo@gmail.com>
2020-04-13 18:17:27 +0900

    impl Queryable for DetailedContest<BriefPoll>

--- backend/crop_domain/src/contest/model/detailed.rs
+++ backend/crop_domain/src/contest/model/detailed.rs
@@ -1,5 +1,7 @@
-use crate::contest::poll::model::Poll;
+use crate::contest::poll::{BriefPoll, Poll};
+use crate::contest::Queryable;
 use chrono::{DateTime, Utc};
+use crop_infra::pg::{choice::ChoiceTable, contest::ContestTable, poll::PollTable, Connection};
 
 use super::{Contest, ContestId, ContestStatus, WithAttrs, WithPoll};
 
@@ -49,5 +51,35 @@ where
 
     fn _current_poll(&self) -> Option<&P> {
         self.poll.as_ref()
+    }
+}
+
+impl Queryable for DetailedContest<BriefPoll> {
+    fn query_by_id(conn: &Connection, id: &ContestId) -> anyhow::Result<Option<Self>> {
+        let contest = match ContestTable::query_by_id(conn, &id.0)? {
+            Some(contest) => contest,
+            None => return Ok(None),
+        };
+
+        if let Some(poll) = PollTable::query_not_resolved_by_contest_id(conn, &id.0)? {
+            let choices = ChoiceTable::query_by_poll_id(conn, &poll.id)?;
+            Ok(Some(DetailedContest {
+                id: *id,
+                status: contest.status,
+                title: contest.title,
+                category: contest.category,
+                event_start_at: contest.event_start_at,
+                poll: Some(BriefPoll::from((poll, choices))),
+            }))
+        } else {
+            Ok(Some(DetailedContest {
+                id: *id,
+                status: contest.status,
+                title: contest.title,
+                category: contest.category,
+                event_start_at: contest.event_start_at,
+                poll: None,
+            }))
+        }
     }
 }
