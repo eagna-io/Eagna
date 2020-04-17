0bbf4b622c10c6f8590975be79407d0812d573bc
AtsukiTak <takatomgoo@gmail.com>
2020-04-18 01:28:18 +0900

    Pollの自動close

--- backend/crop_server/src/routes/contests/_id/polls/post.rs
+++ backend/crop_server/src/routes/contests/_id/polls/post.rs
@@ -38,6 +38,7 @@ pub fn route(ctx: Context) -> impl Filter<Extract = (Response,), Error = Rejecti
 }
 
 async fn inner(ctx: Context, contest_id: ContestId, body: ReqBody) -> Result<Response, Error> {
+    // PollをDBに追加する
     let poll = ctx
         .pg
         .with_conn::<Result<poll::New, Error>, _>(move |conn| {
@@ -53,10 +54,53 @@ async fn inner(ctx: Context, contest_id: ContestId, body: ReqBody) -> Result<Res
 
     let poll_id = *poll.id();
 
+    // 指定時間後にPollをCloseする
+    if let Some(dur) = poll.duration() {
+        let _ = tokio::spawn(start_close_timer(ctx.clone(), *dur, contest_id, poll_id));
+    }
+
+    // 追加したPollをブロードキャストする
     let msg_source = PollMsgSource::from(poll);
     ctx.contest_manager
         .broadcast_msg(contest_id, msg_source)
         .await;
 
     Ok(response::new(StatusCode::CREATED, &ResBody(&poll_id)))
+}
+
+async fn start_close_timer(ctx: Context, dur: Duration, contest_id: ContestId, poll_id: PollId) {
+    // Closeするまで待つ
+    let tokio_dur = tokio::time::Duration::from_secs(dur.num_seconds() as u64);
+    tokio::time::delay_for(tokio_dur).await;
+
+    close_poll(ctx, contest_id, poll_id).await;
+}
+
+async fn close_poll(ctx: Context, contest_id: ContestId, poll_id: PollId) {
+    let msg_source = ctx
+        .pg
+        .with_conn(move |conn| {
+            // TODO: DetailedContestである必要ない。MinumumContestでいい。
+            let contest = conn
+                .query_by_id::<DetailedContest<DetailedPoll>>(&contest_id)
+                .unwrap()
+                .unwrap();
+            let poll = contest.current_poll().unwrap();
+
+            if *poll.id() != poll_id {
+                panic!("A new poll is created while prev poll is not closed");
+            }
+
+            let closed = poll.clone().close().unwrap();
+            conn.save(&closed).unwrap();
+
+            PollMsgSource::from(closed)
+        })
+        .await
+        .unwrap();
+
+    // CloseMsgをブロードキャスト
+    ctx.contest_manager
+        .broadcast_msg(contest_id, msg_source)
+        .await;
 }
