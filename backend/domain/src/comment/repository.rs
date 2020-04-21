caaccba90fef9e644e6ae57019f89961b2da67dc
AtsukiTak <takatomgoo@gmail.com>
2020-04-21 14:58:02 +0900

    add ContestRepository, CommentRepository

---
+++ backend/domain/src/comment/repository.rs
@@ -0,0 +1,7 @@
+use super::Comment;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait CommentRepository {
+    async fn save(&mut self, comment: &Comment) -> anyhow::Result<()>;
+}
--- backend/domain/src/contest/mod.rs
+++ backend/domain/src/contest/mod.rs
@@ -1,5 +1,7 @@
 pub mod model;
 pub mod poll;
+pub mod repository;
 
 pub use model::*;
 pub use poll::*;
+pub use repository::*;
